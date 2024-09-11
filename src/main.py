af325ac93e7dac1a79560db29e42a1567dd5d544
MaskedGod <codegobbledygook@gmail.com>
2024-09-12 01:15:32 +0400

    remove health endpoint from auth router

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from fastapi import Depends, FastAPI, status
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
+# from apscheduler.schedulers.asyncio import AsyncIOScheduler
+# from contextlib import asynccontextmanager
+
 from .database import get_session
 
 from .task.router import task_router
@@ -12,6 +15,26 @@ app = FastAPI(title="TaskMate")
 
 app.include_router(task_router)
 app.include_router(auth_router)
+
+# scheduler = AsyncIOScheduler()
+
+
+# async def check_task_expiry():
+#     # logic for checking and updating expired tasks
+#     print("Checking for expired tasks...")
+
+
+# @asynccontextmanager
+# async def lifespan(app: FastAPI):
+#     scheduler.add_job(check_task_expiry, "interval", minutes=60)
+#     scheduler.start()
+#     print("Scheduler started")
+#     yield
+#     print("Shutting down scheduler")
+#     scheduler.shutdown()
+
+
+# app.router.lifespan_context = lifespan
 
 
 @app.get("/health", status_code=status.HTTP_200_OK)
--- src/task/schemas.py
+++ src/task/schemas.py
@@ -1,3 +1,4 @@
+from datetime import date
 from enum import Enum
 from pydantic import BaseModel
 
@@ -7,6 +8,7 @@ class TaskStatus(str, Enum):
     pending = "pending"
     in_progress = "in-progress"
     completed = "completed"
+    # overdue = "overdue"
 
 
 class CreateTask(BaseModel):
@@ -14,6 +16,7 @@ class CreateTask(BaseModel):
     title: str
     description: str
     status: TaskStatus
+    # due_date: date | None
 
 
 class DisplayTask(BaseModel):
