babb1eda4fe19dbe6af6759cc2fba3e16e2b2b8b
MaskedGod <codegobbledygook@gmail.com>
2024-10-02 17:25:10 +0400

    env

--- src/config.py
+++ src/config.py
@@ -21,14 +21,13 @@ class Settings(BaseSettings):
 
         return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
-
     @property
     def test_database_url(self):
 
         return f"postgresql+asyncpg://{self.TEST_DB_USER}:{self.TEST_DB_PASS}@{self.TEST_DB_HOST}:{self.TEST_DB_PORT}/{self.TEST_DB_NAME}"
 
-    model_config = SettingsConfigDict(env_file=".env")
+    # prod env
+    model_config = SettingsConfigDict(env_file="/etcsecrets/.env")
 
 
 settings = Settings()
